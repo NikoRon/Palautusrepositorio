ccf2e626c676352fc69daa8c012a0fd5b23554ea
Niko Rönkkö <nikopetteri.ronkko@dnainternet.net>
2024-11-09 13:50:51 +0200

    kurssitietojen parempi järjestely

--- Osa1/Kurssitiedot/src/App.jsx
+++ Osa1/Kurssitiedot/src/App.jsx
@@ -1,33 +1,42 @@
-
 // Kurssin nimen näyttäminen
 const Header = ({ course }) => <h1>{course}</h1>;
+
 // Kurssin sisällön näyttäminen
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => (
+const Content = ({ parts }) => (
   <div>
-    <p>{part1} {exercises1}</p>
-    <p>{part2} {exercises2}</p>
-    <p>{part3} {exercises3}</p>
+    {parts.map((part, index) => (
+      <p key={index}>{part.name} {part.exercises}</p>
+    ))}
   </div>
 );
-// Kurssin tehtävien pisteiden näyttäminen
-const Total = ({ exercises1, exercises2, exercises3 }) => (
-  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+// Muutoksen jälkeen, total laskee nyt olioiden kautta.
+const Total = ({ parts }) => (
+  <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
 );
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
