6eb461d273397e7ecbbca1da67d604c1f52e4d6d
Niko Rönkkö <nikopetteri.ronkko@dnainternet.net>
2024-12-10 17:35:35 +0200

    välipalautus, 2.5 tehtynä, erillinen moduuli courselle

--- Osa2/Kurssitiedot/src/App.jsx
+++ Osa2/Kurssitiedot/src/App.jsx
@@ -1,47 +1,76 @@
-// Kurssin nimen näyttäminen
-const Header = ({ course }) => <h1>{course}</h1>;
-
-// Kurssin sisällön näyttäminen
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part, index) => (
-      <p key={index}>{part.name} {part.exercises}</p>
-    ))}
-  </div>
-);
-
-// Muutoksen jälkeen, total laskee nyt olioiden kautta.
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {totalExercises}</p>;
-};
+import Course from './components/Course';
+import React from 'react';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: ' uusi testiosio',
+          exercises: 6,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'kolmas kurssi',
+      id: 3,
+      parts: [
+        {
+          name: 'testiosio',
+          exercises: 1,
+          id: 1
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ Osa2/Kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+//Course.jsx:ään siirretään kaikki sen alikomponentit eli 
+//alla olevat header, content ja total
+const Course = ({ course }) => {
+    return(
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    );
+  };
+
+//h2 muokkauksessa pienempi otsikko
+const Header = ({ course }) => <h2>{course}</h2>;
+
+
+const Content = ({ parts }) => (
+    <div>
+      {parts.map((part, index) => {
+        console.log(part);
+        return (
+          <p key={index}>
+            {part.name} {part.exercises}
+          </p>
+        );
+      })}
+    </div>
+  );
+  
+
+
+const Total = ({ parts }) => {
+    const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return <p><b>total of {totalExercises} exercises</b></p>;
+};
+
+export default Course;
